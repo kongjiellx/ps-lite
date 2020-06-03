09372dbc6b88305130fc9f70543c640ead048171
liuyekuan@kanzhun.com <liuyekuan@kanzhun.com>
2020-06-03 21:08:04 +0800

    add deps

--- make/deps.mk
+++ make/deps.mk
@@ -1,51 +1,45 @@
 # Install dependencies
 
-URL1=https://raw.githubusercontent.com/mli/deps/master/build
-URL2=https://github.com/google/protobuf/releases/download/v3.8.0
-ifndef WGET
-WGET = wget
-endif
-
 # protobuf
 PROTOBUF = ${DEPS_PATH}/include/google/protobuf/message.h
 ${PROTOBUF}:
-	$(eval FILE=protobuf-cpp-3.8.0.tar.gz)
+	$(eval FILE=third_party/protobuf-cpp-3.8.0.tar.gz)
 	$(eval DIR=protobuf-3.8.0)
-	rm -rf $(FILE) $(DIR)
-	$(WGET) $(URL2)/$(FILE) && tar --no-same-owner -zxf $(FILE)
+	rm -rf $(DIR)
+	tar --no-same-owner -zxf $(FILE)
 	cd $(DIR) && export CFLAGS=-fPIC && export CXXFLAGS=-fPIC && ./configure -prefix=$(DEPS_PATH) && $(MAKE) && $(MAKE) install
-	rm -rf $(FILE) $(DIR)
+	rm -rf $(DIR)
 
 # zmq
 ZMQ = ${DEPS_PATH}/include/zmq.h
 
 ${ZMQ}:
-	$(eval FILE=zeromq-4.1.4.tar.gz)
+	$(eval FILE=third_party/zeromq-4.1.4.tar.gz)
 	$(eval DIR=zeromq-4.1.4)
-	rm -rf $(FILE) $(DIR)
-	$(WGET) $(URL1)/$(FILE) && tar --no-same-owner -zxf $(FILE)
+	rm -rf $(DIR)
+	tar --no-same-owner -zxf $(FILE)
 	cd $(DIR) && export CFLAGS=-fPIC && export CXXFLAGS=-fPIC && ./configure -prefix=$(DEPS_PATH) --with-libsodium=yes --with-libgssapi_krb5=no && $(MAKE) && $(MAKE) install
-	rm -rf $(FILE) $(DIR)
+	rm -rf $(DIR)
 
 # lz4
 LZ4 = ${DEPS_PATH}/include/lz4.h
 ${LZ4}:
-	$(eval FILE=lz4-r129.tar.gz)
+	$(eval FILE=third_party/lz4-r129.tar.gz)
 	$(eval DIR=lz4-r129)
-	rm -rf $(FILE) $(DIR)
-	wget $(URL1)/$(FILE) && tar --no-same-owner -zxf $(FILE)
+	rm -rf $(DIR)
+	tar --no-same-owner -zxf $(FILE)
 	cd $(DIR) && $(MAKE) && PREFIX=$(DEPS_PATH) $(MAKE) install
-	rm -rf $(FILE) $(DIR)
+	rm -rf $(DIR)
 
 # cityhash
 CITYHASH = ${DEPS_PATH}/include/city.h
 ${CITYHASH}:
-	$(eval FILE=cityhash-1.1.1.tar.gz)
+	$(eval FILE=third_party/cityhash-1.1.1.tar.gz)
 	$(eval DIR=cityhash-1.1.1)
-	rm -rf $(FILE) $(DIR)
-	wget $(URL1)/$(FILE) && tar --no-same-owner -zxf $(FILE)
+	rm -rf $(DIR)
+	tar --no-same-owner -zxf $(FILE)
 	cd $(DIR) && ./configure -prefix=$(DEPS_PATH) --enable-sse4.2 && $(MAKE) CXXFLAGS="-g -O3 -msse4.2" && $(MAKE) install
-	rm -rf $(FILE) $(DIR)
+	rm -rf $(DIR)
 
 
 # # gflags
